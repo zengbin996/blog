3d73f6e2eb425c6cd8a38b83576cade45fda6ca5
zengbin <hellozengbin@qq.com>
2023-05-11 18:07:43 +0800

    cos 上传

--- docs/.vuepress/pages/utils/Index.vue
+++ docs/.vuepress/pages/utils/Index.vue
@@ -5,23 +5,23 @@
     <div>
       <h2>上传文件获取在线链接</h2>
 
-      <input type="file" @change="uploadFile" ref="input" />
+      <input type="file" @change="uploadFile" ref="input" multiple />
 
       <div v-if="url">
-        <span>{{ url }}</span> <button>复制地址</button>
+        <div>
+          <img :src="url" alt="" style="width: 300px" />
+        </div>
+        <span>{{ url }}</span> <button @click="() => copyTextToClipboard(url)">复制地址</button>
       </div>
     </div>
+
+    <button @click="deleteFiles('/')">删除文件</button>
   </div>
 </template>
 
 <script>
 import COS from 'cos-js-sdk-v5';
 import { v4 as uuidv4 } from 'uuid';
-
-const cos = new COS({
-  SecretId: 'AKID5zd1way5vTklUw71e070dz5zcx1nJXVB',
-  SecretKey: 'QaT5SZPyGI3XkgLjpfDUTBdoENGsDH3Z',
-});
 
 export default {
   data() {
@@ -30,14 +30,14 @@ export default {
     };
   },
   methods: {
-    //上次文件
+    //上传文件
     uploadFile() {
       const file = this.$refs.input.files[0];
       const key = `${uuidv4()}.${file.name.split('.').pop()}`; //文件目录及文件名称
 
       cos.putObject(
         {
-          Bucket: 'blog-zengbin-1258070316', //填入您自己的存储桶，必须字段
+          Bucket: 'blog-dist-1258070316', //填入您自己的存储桶，必须字段
           Region: 'ap-chengdu', //存储桶所在地域，必须字段
           Key: key, //存储在桶里的对象键，必须字段
           Body: file, //上传文件对象，必须字段
@@ -46,6 +46,54 @@ export default {
           if (err) return console.log('上传失败');
 
           this.url = 'https://' + data.Location;
+        }
+      );
+    },
+
+    //复制
+    copyTextToClipboard(text) {
+      const textarea = document.createElement('textarea');
+      textarea.value = text;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+    },
+
+    //删除
+
+    deleteFiles(marker) {
+      cos.getBucket(
+        {
+          Bucket: 'blog-dist-1258070316' /* 填入您自己的存储桶，必须字段 */,
+          Region: 'ap-chengdu' /* 存储桶所在地域，例如ap-beijing，必须字段 */,
+          Prefix: 'a/',
+          Marker: marker,
+          MaxKeys: 1000,
+        },
+        function (listError, listResult) {
+          if (listError) return console.log('list error:', listError);
+          var nextMarker = listResult.NextMarker;
+          var objects = listResult.Contents.map(function (item) {
+            return { Key: item.Key };
+          });
+          cos.deleteMultipleObject(
+            {
+              Bucket: 'blog-dist-1258070316' /* 填入您自己的存储桶，必须字段 */,
+              Region: 'ap-chengdu' /* 存储桶所在地域，例如ap-beijing，必须字段 */,
+              Objects: objects,
+            },
+            function (delError, deleteResult) {
+              if (delError) {
+                console.log('delete error', delError);
+                console.log('delete stop');
+              } else {
+                console.log('delete result', deleteResult);
+                if (listResult.IsTruncated === 'true') deleteFiles(nextMarker);
+                else console.log('delete complete');
+              }
+            }
+          );
         }
       );
     },
---
+++ serve/cos-deploy.js
@@ -0,0 +1,77 @@
+const COS = require('cos-nodejs-sdk-v5');
+const fs = require('fs');
+const path = require('path');
+
+const Bucket = 'blog-dist-1258070316';
+const Region = 'ap-chengdu';
+
+const deleteFiles = function (marker) {
+  cos.getBucket(
+    {
+      Bucket,
+      Region,
+      MaxKeys: 1000,
+    },
+    function (listError, listResult) {
+      if (listError) return console.log('list error:', listError);
+      var nextMarker = listResult.NextMarker;
+      var objects = listResult.Contents.map(function (item) {
+        return { Key: item.Key };
+      });
+      cos.deleteMultipleObject(
+        {
+          Bucket,
+          Region,
+          Objects: objects,
+        },
+        function (delError, deleteResult) {
+          if (delError) {
+            console.log('delete error', delError);
+            console.log('delete stop');
+          } else {
+            console.log('delete result', deleteResult);
+            if (listResult.IsTruncated === 'true') deleteFiles(nextMarker);
+            else console.log('delete complete');
+          }
+        }
+      );
+    }
+  );
+};
+
+function uploadFiles(dir) {
+  const files = fs.readdirSync(dir);
+
+  files.forEach((file) => {
+    const filePath = path.join(dir, file);
+    const fileStat = fs.statSync(filePath);
+
+    if (fileStat.isDirectory()) {
+      uploadFiles(filePath);
+    } else {
+      const key = filePath
+        .replace(__dirname, '')
+        .replace(/\\/g, '/')
+        .replace(/^.*?\.vuepress\/dist\//, '/');
+      cos.putObject(
+        {
+          Bucket,
+          Region,
+          Key: key,
+          Body: fs.createReadStream(filePath),
+        },
+        function (err, data) {
+          if (err) {
+            console.error(`Upload ${key} failed:`, err);
+          } else {
+            console.log(`Upload ${key} successfully.`);
+          }
+        }
+      );
+    }
+  });
+}
+
+uploadFiles(path.resolve(__dirname, '../docs/.vuepress/dist'));
+
+// deleteFiles();
