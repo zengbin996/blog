bf292a723ca7797b938af64e9527d8e0251b4c27
zengbin <hellozengbin@qq.com>
2023-05-15 11:48:11 +0800

    打包配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,7 @@ import sidebarConfig from './config/sidebarConfig';
 export default defineUserConfig({
   title: '一条有梦想的咸鱼',
   port: '9906',
+  base: process.env.base || '/',
   head: [['link', { rel: 'icon', href: 'https://blog-zengbin-1258070316.cos.ap-chengdu.myqcloud.com/ico.svg' }]],
   theme: defaultTheme({
     logo: 'https://blog-zengbin-1258070316.cos.ap-chengdu.myqcloud.com/ico.svg',
