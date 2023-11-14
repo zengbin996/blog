be251e024dc3d2a59192d1b983ce4bd2da9b8dad
zengbin <hellozengbin@qq.com>
2023-11-14 18:22:39 +0800

    日曜日、李さんは公園でボートに乗りました。

--- docs/.vuepress/config/sidebarConfig.js
+++ docs/.vuepress/config/sidebarConfig.js
@@ -213,6 +213,7 @@ export default {
         { text: '总结 3', link: '/jp/grammar/unit12-2' },
         { text: '第 13 课', link: '/jp/grammar/unit13' },
         { text: '第 14 课', link: '/jp/grammar/unit14' },
+        { text: '第 15 课', link: '/jp/grammar/unit15' },
       ],
     },
   ],
